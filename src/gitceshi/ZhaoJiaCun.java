efbd42bb501bb8b84df335067bba202a9593b20d
赵家存 <@LAPTOP-9V6KHSE8>
2019-04-23 16:18:13 +0800

    进行一次修改

--- src/gitceshi/ZhaoJiaCun.java
+++ src/gitceshi/ZhaoJiaCun.java
@@ -4,7 +4,7 @@ public class ZhaoJiaCun {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("你好，赵家存");
+		System.out.println("你好，赵家存,进行一次修改");
 	}
 
 }
